ac8bc51875dbf7d93548a8456dec46367c46baf3
Li Jung Huang <lijunghuang@Lis-MacBook-Pro.local>
2020-04-13 13:30:06 -0700

    mini

--- responive-mini/src/components/Banner.js
+++ responive-mini/src/components/Banner.js
@@ -1,16 +1,49 @@
 import React from 'react';
 
-const Banner = (props) => {
-    return (
-        <div className='banner-container'>
-            <div>
-                {/* <h1>HAMBURGERS</h1> */}
-                <h2>Welcome To Our Studio!</h2>
-                <h1>IT'S NICE TO MEET YOU</h1>
-                <button>TELL ME MORE</button>
-            </div>
-        </div>
-    );
-};
+class Banner extends React.Component {
+    constructor(){
+        super()
+        this.state={
+            toggleSideBar:false
+        }
+    }
+    toggleSideBarFunc(){
+        this.setState((prevState)=>{
+            return{
+                toggleSideBar: !prevState.toggleSideBar
+            } 
+        })
+    }
+    
+    render(){
+
+        return (
+            <header>
+                <div>
+                    <div className="logo">
+                        <a href="#">Start Bootstrap</a>
+                    </div>
+                    <button onClick={()=>this.toggleSideBarFunc()}>Click!</button>
+                    <nav className={this.state.toggleSideBar? "show":""}>
+                        <ul>
+                            <li>
+                                <a href="#/services">Services</a>
+                            </li>
+                            <li>
+                                <a href="#/portfolio">Portfolio</a>
+                            </li>
+                            <li>
+                                <a href="#/about">About</a>
+                            </li>
+                            <li>
+                                <a href="#/team">Team</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        );
+    };
+ }
 
 export default Banner;
